6f6ff41e559505dac720d5af4a6cfafab624379e
waleedtarbosh <waleedtarbush@gmail.com>
2023-04-11 01:57:04 +0300

    SideBar and SearchBar  merged with Components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import CarouselMain from './components/Carousel/CarouselMain.jsx';
+import CarouselMain from './Components/Carousel/CarouselMain.jsx';
 import SearchBarandIcons from './Components';
-import CenteredTabs from "./components/Tabs/index";
+import CenteredTabs from "./Components/Tabs/index";
 import SideBar from './Components/SideBar';
 function App() {
   return  <div className="App">
