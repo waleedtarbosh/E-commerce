0b2e63ba497ba1bf1ca13a3226a7fbd785459ad4
waleedtarbosh <waleedtarbush@gmail.com>
2023-04-10 15:07:38 +0300

    Carousel merged with Tabs

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import CarouselMain from './components/Carousel/CarouselMain.jsx';
-import CenteredTabs from "./component/Tabs/index";
+import CenteredTabs from "./components/Tabs/index";
 
 function App() {
   return  <div className="App">
