bcebb52bec368fa328ac118a797acff3a60cc3ab
waleedtarbosh <waleedtarbush@gmail.com>
2023-04-17 15:08:11 +0300

    Update Countdown component to use unique keys

--- src/Components/FlashSalesCountdownSection/Countdown/Countdown.js
+++ src/Components/FlashSalesCountdownSection/Countdown/Countdown.js
@@ -1,27 +1,31 @@
 import React from 'react';
 import styles from './Countdown.module.css';
+
 const Countdown = ({ days, hours, minutes, seconds }) => {
   const countdownUnits = [
-    { label: 'Days', value: days, counterClass: styles.DaysCounter, labelClass: styles.days, valueClass: styles.heading },
-    { label: 'Hours', value: hours, counterClass: styles.HoursCounter, labelClass: styles.hours, valueClass: styles.heading },
-    { label: 'Minutes', value: minutes, counterClass: styles.MinutesCounter, labelClass: styles.minutes, valueClass: styles.heading },
-    { label: 'Seconds', value: seconds, counterClass: styles.SecondsCounter, labelClass: styles.seconds, valueClass: styles.heading }
+    { id: 'days', label: 'Days', value: days, counterClass: styles.DaysCounter, labelClass: styles.days, valueClass: styles.heading },
+    { id: 'hours', label: 'Hours', value: hours, counterClass: styles.HoursCounter, labelClass: styles.hours, valueClass: styles.heading },
+    { id: 'minutes', label: 'Minutes', value: minutes, counterClass: styles.MinutesCounter, labelClass: styles.minutes, valueClass: styles.heading },
+    { id: 'seconds', label: 'Seconds', value: seconds, counterClass: styles.SecondsCounter, labelClass: styles.seconds, valueClass: styles.heading }
   ];
+
   const semiCircleClasses = [
-    { cloneClass: styles.semiClone1, ellipse1: styles.ellipse18, ellipse2: styles.ellipse19 },
-    { cloneClass: styles.semiClone2, ellipse1: styles.ellipse19, ellipse2: styles.ellipse20 },
-    { cloneClass: styles.semiClone3, ellipse1: styles.ellipse21, ellipse2: styles.ellipse22 }
+    { id: 'semiCircle1', cloneClass: styles.semiClone1, ellipse1: styles.ellipse18, ellipse2: styles.ellipse19 },
+    { id: 'semiCircle2', cloneClass: styles.semiClone2, ellipse1: styles.ellipse19, ellipse2: styles.ellipse20 },
+    { id: 'semiCircle3', cloneClass: styles.semiClone3, ellipse1: styles.ellipse21, ellipse2: styles.ellipse22 }
   ];
+
   return (
     <div className={styles.group}>
-      {semiCircleClasses.map((circle, index) => (
-        <div className={`${circle.cloneClass}`} key={index}>
+      {semiCircleClasses.map((circle) => (
+        <div className={`${circle.cloneClass}`} key={circle.id}>
           <div className={`${circle.ellipse1}`}></div>
           <div className={`${circle.ellipse2}`}></div>
         </div>
-      ))}  
-      {countdownUnits.map((unit, index) => (
-        <div className={`${styles.counter} ${unit.counterClass}`} key={index}>
+      ))}
+      
+      {countdownUnits.map((unit) => (
+        <div className={`${styles.counter} ${unit.counterClass}`} key={unit.id}>
           <div className={`${styles.label} ${unit.labelClass}`}>{unit.label}</div>
           <div className={` ${unit.valueClass}`}>{unit.value.toString().padStart(2, '0')}</div>
         </div>
@@ -29,4 +33,5 @@ const Countdown = ({ days, hours, minutes, seconds }) => {
     </div>
   );
 }
+
 export default Countdown;
