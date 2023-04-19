e47867f87fcecff4c01349095cd4f6b6056079fa
iamhaitham <iamhaitham2011@gmail.com>
2023-04-19 21:10:27 +0200

    Display the sub-sections of the homepage

--- src/Components/HomePage/HomePage.js
+++ src/Components/HomePage/HomePage.js
@@ -1,13 +1,23 @@
 import CarouselMain from "./Carousel/CarouselMain";
 import SideBar from "./SideBar";
 import styles from "./HomePage.module.css";
+import FlashSales from "./Sections/FlashSales/FlashSales";
+import BestSelling from "./Sections/BestSelling/BestSelling";
+import ExploreOurProducts from "./Sections/ExploreOurProducts/ExploreOurProducts";
+import BrowseByCategory from "./Sections/BrowseByCategory/BrowseByCategory";
+
+
 const HomePage = () => {
     return (
-     <div className={styles.HomePage}>
-      <SideBar/>
-      <CarouselMain/>
-     </div>
-    )
+      <div className={styles.HomePage}>
+        <SideBar/>
+        <CarouselMain/>
+        {/* <FlashSales/>
+        <BrowseByCategory/>
+        <BestSelling/>
+        <ExploreOurProducts/> */}
+      </div>
+    );
   }
-  
-  export default HomePage;
+
+export default HomePage;
