8934b3111cf237818fc8e21591b3ce60c5af50f8
mahmoud <mm@gmail.com>
2023-04-24 21:05:43 +0300

    Modify the style

--- src/Components/HomePage/Carousel/CarouselMain.js
+++ src/Components/HomePage/Carousel/CarouselMain.js
@@ -1,29 +1,29 @@
-import React from 'react';
-import Carousel from 'react-material-ui-carousel';
-import { Box } from '@mui/material';
-import styles from './CarouselMain.module.css';
+import React from "react";
+import Carousel from "react-material-ui-carousel";
+import { Box } from "@mui/material";
+import styles from "./CarouselMain.module.css";
 
 function CarouselMain() {
   const items = [
     {
-      src: 'https://fakestoreapi.com/img/61IBBVJvSDL._AC_SY879_.jpg',
-      alt: 'Image 3',
-      caption: 'Electronics',
+      src: "https://fakestoreapi.com/img/61IBBVJvSDL._AC_SY879_.jpg",
+      alt: "Image 3",
+      caption: "Electronics",
     },
     {
-      src: 'https://fakestoreapi.com/img/71pWzhdJNwL._AC_UL640_QL65_ML3_.jpg',
-      alt: 'Image 2',
-      caption: 'Jewelery',
+      src: "https://fakestoreapi.com/img/71pWzhdJNwL._AC_UL640_QL65_ML3_.jpg",
+      alt: "Image 2",
+      caption: "Jewelery",
     },
     {
-      src: 'https://fakestoreapi.com/img/51Y5NI-I5jL._AC_UX679_.jpg',
-      alt: 'Image 4',
-      caption: 'womens clothing',
+      src: "https://fakestoreapi.com/img/51Y5NI-I5jL._AC_UX679_.jpg",
+      alt: "Image 4",
+      caption: "womens clothing",
     },
     {
-      src: 'https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg',
-      alt: 'Image 1',
-      caption: 'mens clothing',
+      src: "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg",
+      alt: "Image 1",
+      caption: "mens clothing",
     },
   ];
 
@@ -38,9 +38,19 @@ function CarouselMain() {
         navButtonsAlwaysVisible={false}
       >
         {items.map((item, index) => (
-          <Box key={index} display="flex" flexDirection="row-reverse" alignItems="center" justifyContent="space-between">
-            <img src={item.src} alt={item.alt} className={styles.carouselImage} />
-           
+          <Box
+            key={index}
+            display="flex"
+            flexDirection="row-reverse"
+            alignItems="center"
+            justifyContent="space-between"
+          >
+            <img
+              src={item.src}
+              alt={item.alt}
+              className={styles.carouselImage}
+            />
+            <p className={styles.carouselCaption}>{item.caption}</p>
           </Box>
         ))}
       </Carousel>
